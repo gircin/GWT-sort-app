88ba63189f321a713279b60378f4ffb964b32de3
gircin <39699829+gircin@users.noreply.github.com>
2020-12-10 12:55:57 +0200

    Update AppModule.java
    Add comment

--- src/main/java/com/client/AppModule.java
+++ src/main/java/com/client/AppModule.java
@@ -3,6 +3,10 @@ package com.client;
 import com.client.view.InputView;
 import com.google.gwt.core.client.EntryPoint;
 
+/*
+* Entry point.
+* The class creates an UI with inputting numbers
+*/
 public class AppModule implements EntryPoint {
     public void onModuleLoad() {
 
